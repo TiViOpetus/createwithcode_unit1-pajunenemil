ec263121053f8c88eff8350531286bb84bd3fae6
pajunenemil <97280425+pajunenemil@users.noreply.github.com>
2022-01-27 12:14:03 +0200

    uusi kamera lisätty ja vihollisia

--- Unit(1)/Assets/script/camrecontroller.cs
+++ Unit(1)/Assets/script/camrecontroller.cs
@@ -1,21 +1,79 @@
 using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
+
+
 public class camrecontroller : MonoBehaviour
+
 {
-    private Vector3 offset;
 
     public GameObject player;
+
+    public GameObject altCamera;
+
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    public int playerIndex;
+
     // Start is called before the first frame update
+
+
+
     void Start()
+
     {
-        offset = transform.position;
+
+
+
     }
 
+
+
     // Update is called once per frame
+
     void LateUpdate()
+
     {
+
         transform.position = player.transform.position + offset;
+
     }
+
+
+
+    private void Update()
+
+    {
+
+        if ((playerIndex == 0 && Input.GetKeyDown(KeyCode.V)) || (playerIndex == 1 && Input.GetKeyDown(KeyCode.B)))
+
+        {
+
+            if (GetComponent<Camera>().isActiveAndEnabled)
+
+            {
+
+                altCamera.SetActive(true);
+
+                GetComponent<Camera>().enabled = false;
+
+            }
+
+            else
+
+            {
+
+                altCamera.SetActive(false);
+
+                GetComponent<Camera>().enabled = true;
+
+            }
+
+        }
+
+    }
+
 }
